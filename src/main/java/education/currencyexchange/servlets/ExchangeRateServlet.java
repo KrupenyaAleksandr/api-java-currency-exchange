6b1927fa851e4fd977043315581c516be5b2a510
sasha <elshurimem1337@gmail.com>
2024-08-04 16:00:13 +0300

    update gitignore

---
+++ src/main/java/education/currencyexchange/servlets/ExchangeRateServlet.java
@@ -0,0 +1,9 @@
+package education.currencyexchange.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/exchange-rate/*")
+public class ExchangeRateServlet extends HttpServlet {
+
+}
